1b3c244634e914025535e18c430f1d62d7ad121b
ccsplit <cshay237@gmail.com>
2019-03-15 14:46:29 -0500

    Add some simple socket server and client functionality.

---
+++ sepulcrum/socket/client.py
@@ -0,0 +1,34 @@
+from ..logging import get_logger
+import socket
+import ssl
+
+
+class Client(object):
+    def __init__(self, host, port, tls=True, encoding="UTF-8"):
+        self.logger = get_logger(self.__class__.__name__)
+        self.logger.debug("Creating socket to {}:{}".format(host, port))
+        if tls:
+            self.sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS)
+            self.sslcontext.verify_mode = ssl.CERT_NONE
+            self.sslcontext.check_hostname = False
+            self.socket = self.sslcontext.wrap_socket(
+                socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            )
+        else:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
+
+    def send(self, text):
+        self.logger.debug("Sending message: '{}'".format(text))
+        self.socket.sendall("{}\n".format(text).encode(self.encoding))
+
+    def recv(self, length=1024):
+        self.logger.debug("Receiving message")
+        self.response = self.socket.recv(length)
+        self.logger.info(
+            "Received: '{}'".format(self.response).encode(self.encoding)
+        )  # noqa: E501
+
+    def close(self):
+        self.logger.debug("Closing socket.")
+        self.socket.close()
---
+++ sepulcrum/socket/server.py
@@ -0,0 +1,67 @@
+from ..logging import get_logger
+import os
+
+try:
+    import SocketServer
+except ImportError:
+    # Python 3
+    import socketserver as SocketServer
+
+
+class BaseRequestHandler(SocketServer.BaseRequestHandler):
+    def __init__(
+        self, request, client_address, server, encoding="UTF-8", receive_size=1024
+    ):
+        super(BaseRequestHandler, self).__init__(self, request, client_address, server)
+        self.logger = get_logger(self.__class__.__name__)
+        self.encoding = encoding
+        self.recv_size = receive_size
+
+    def send_msg(self, msg):
+        self.request.sendall(msg.encode(self.encoding))
+
+    def recv_msg(self):
+        return self.request.recv(self.recv_size).strip().decode(self.encoding)
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+
+class ThreadedSSLTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    def __init__(
+        self,
+        server_address,
+        RequestHandlerClass,
+        bind_and_activate=True,
+        ssl_key_path=None,
+        ssl_cert_path=None,
+    ):
+        """Constructor. May be extended, do not override."""
+        SocketServer.TCPServer.__init__(
+            self, server_address, RequestHandlerClass, False
+        )
+
+        dir = os.path.dirname(__file__)
+        if ssl_key_path is None:
+            key_file = os.path.join(dir, "server.key")
+        elif not os.path.exists(ssl_key_path):
+            raise ValueError("SSL key path {} does not exist.".format(ssl_key_path))
+        else:
+            key_file = ssl_key_path
+        if ssl_cert_path is None:
+            cert_file = os.path.join(dir, "server.crt")
+        elif not os.path.exists(ssl_cert_path):
+            raise ValueError("SSL cert path {} does not exist.".format(ssl_cert_path))
+        else:
+            cert_file = ssl_cert_path
+
+        import ssl
+
+        self.socket = ssl.wrap_socket(
+            self.socket, keyfile=key_file, certfile=cert_file, cert_reqs=ssl.CERT_NONE
+        )
+
+        if bind_and_activate:
+            self.server_bind()
+            self.server_activate()
