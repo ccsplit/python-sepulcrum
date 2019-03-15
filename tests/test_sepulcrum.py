52bf85a0c726102ad8b13530c7c5ad65035d6849
ccsplit <cshay237@gmail.com>
2019-03-15 14:44:10 -0500

    Black formatted the test code.

--- tests/test_sepulcrum.py
+++ tests/test_sepulcrum.py
@@ -1,12 +1,11 @@
-
 from click.testing import CliRunner
 
-from sepulcrum.cli import main
+from .context import sepulcrum
 
 
 def test_main():
     runner = CliRunner()
-    result = runner.invoke(main, [])
+    result = runner.invoke(sepulcrum.cli.main, [])
 
-    assert result.output == '()\n'
+    assert result.output == "()\n"
     assert result.exit_code == 0
