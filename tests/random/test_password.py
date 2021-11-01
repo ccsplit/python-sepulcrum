093e6bce45c6b533d351c571d93d40a9b319a1c5
ccsplit <cshay237@gmail.com>
2021-10-31 22:18:38 -0500

    Fix the functions for lower and upper

--- tests/random/test_password.py
+++ tests/random/test_password.py
@@ -7,7 +7,7 @@ def test_password_symbols():
     result = generate_password()
     if not any([i for i in symbols]):
         assert 1 == 2, "There were no symbols within the password."
-    assert lower(result) != result, "All of the characters were lowercase."
-    assert upper(result) != result, "All of the characters were uppercase."
+    assert result.lower() != result, "All of the characters were lowercase."
+    assert result.upper() != result, "All of the characters were uppercase."
 
 
