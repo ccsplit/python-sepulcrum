002a25329934d9c23b1b06759efd2594b9eab7a0
ccsplit <cshay237@gmail.com>
2021-10-31 21:20:00 -0500

    Add some functions to the new module.

---
+++ sepulcrum/math/__init__.py
@@ -0,0 +1,16 @@
+
+
+def add(x, y):
+    return x+y
+
+def multiply(x, y):
+    return x*y
+
+def multiply_float(x, y):
+    return float(x)*y
+
+def divide_float(x, y):
+    return float(x)/y
+
+def divide(x, y):
+    return x/y
